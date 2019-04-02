e3e07b7a2a45204a50374a9370884efacca7ecec
kharlbaron <inoue.sora@gmail.com>
2019-04-02 00:11:54 -0600

    RAG-2 0. Connected app with mock server. 1. Fetching data from mock server. 2. Showing data fetched.

--- grappoll/src/components/App.js
+++ grappoll/src/components/App.js
@@ -2,25 +2,43 @@ import React, { Component } from 'react';
 import logo from '../resources/images/logo.svg';
 import '../resources/styles/App.css';
 
+import {Query} from "react-apollo";
+import gql from 'graphql-tag';
+
 class App extends Component {
   render() {
+
+    const ExchangeRates = () => (
+        <Query
+            query={gql`
+        {
+            rates(currency: "USD") {
+            currency
+            rate
+            }
+        }
+      `}>
+          {({loading, error, data}) => {
+            if (loading) return <p>Loading</p>;
+            if (error) return <p>Error :( </p>;
+
+            return data.rates.map(({currency, rate}) => (
+                <div key={currency}>
+                  <p>{currency}: {rate}</p>
+                </div>
+            ))
+          }}
+        </Query>
+    );
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <div className="App">
+          <header className="App-header">
+            {/*<img src={logo} className="App-logo" alt="logo" />*/}
+            <h2>My first Apollo app <span role="img" aria-labelledby="rocket">🚀</span></h2>
+            {ExchangeRates()}
+          </header>
+        </div>
     );
   }
 }
--- grappoll/src/index.js
+++ grappoll/src/index.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import './resources/styles/index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+/* Apollo section imports */
+import {ApolloProvider} from "react-apollo";
+import ApolloClient  from 'apollo-boost';
+
+const client = new ApolloClient({
+    uri: "https://48p1r2roz4.sse.codesandbox.io/"
+});
+
+ReactDOM.render(
+    <ApolloProvider client={client}>
+        <App />
+    </ApolloProvider>,
+    document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
