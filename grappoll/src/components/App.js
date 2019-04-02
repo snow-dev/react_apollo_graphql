d53d97dc610c619f0a58c24576e5890a48217293
kharlbaron <inoue.sora@gmail.com>
2019-04-01 23:09:22 -0600

    RAG-1 0. Created initial scaffolding for React App.

--- grappoll/src/components/App.js
+++ grappoll/src/components/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from '../resources/images/logo.svg';
+import '../resources/styles/App.css';
 
 class App extends Component {
   render() {
